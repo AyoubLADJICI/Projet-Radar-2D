dd4c22b375cd992a335e9a02d6d0ccd162b14d03
votre_login <votre_mail>
2025-01-03 18:02:15 +0100

    Mise à jour 3

--- DE10_Lite_Computer_YD/software/NiosII_affichage_obstacle/main.c
+++ DE10_Lite_Computer_YD/software/NiosII_affichage_obstacle/main.c
@@ -7,34 +7,34 @@
 int main() {
     printf("Affichage des obstacles :\n");
     while (1) {
-        // Balayer de 0° à 90°
+        // Balayer de 0ï¿½ ï¿½ 90ï¿½
         for (unsigned int position = 0; position < 511; position++) {
         	//Envoyer la position au servomoteur
             IOWR(AVALON_SERVOMOTEUR_0_BASE, 0, position);
-            //Conversion de la position (en binaire) en degré °
+            //Conversion de la position (en binaire) en degrï¿½ ï¿½
             int angle = (position * 180 / 511);
-            //Lecture de la distance avec le télémètre
+            //Lecture de la distance avec le tï¿½lï¿½mï¿½tre
             unsigned int aller_retour = IORD(AVALON_TELEMETRE_0_BASE, 0);
             unsigned int distance = aller_retour/2;
-            printf("%u° -> %u cm\n", angle, distance);
+            printf("%uï¿½ -> %u cm\n", angle, distance);
             usleep(3000); // Pause pour assurer un mouvement fluide (20 ms)
         }
 
-        // Balayer de 180° à 0°
+        // Balayer de 180ï¿½ ï¿½ 0ï¿½
         for (unsigned int position = 511; position > 0; position--) {
         	//Envoyer la position au servomoteur
         	IOWR(AVALON_SERVOMOTEUR_0_BASE, 0, position);
-        	//Conversion de la position (en binaire) en degré °
+        	//Conversion de la position (en binaire) en degrï¿½ ï¿½
         	int angle = (position * 180 / 511);
-        	//Lecture de la distance avec le télémètre
+        	//Lecture de la distance avec le tï¿½lï¿½mï¿½tre
         	unsigned int aller_retour = IORD(AVALON_TELEMETRE_0_BASE, 0);
         	unsigned int distance = aller_retour/2;
-        	printf("%u° -> %u cm\n", angle, distance);
+        	printf("%uï¿½ -> %u cm\n", angle, distance);
         	usleep(3000); // Pause pour assurer un mouvement fluide (20 ms)
         }
         // Pause avant de recommencer un nouveau balayage
         printf("Fin du programme.\n");
-        printf("Redémarrage dans 1 seconde.\n");
+        printf("Redï¿½marrage dans 1 seconde.\n");
         usleep(1000000);
     }
     return 0;
